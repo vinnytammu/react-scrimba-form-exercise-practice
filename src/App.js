4c016abf36287b2e079b9c440e0cca3e513f81c6
vinnytammu <tammydo@usc.edu>
2021-02-15 22:37:28 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //course section: https://scrimba.com/learn/learnreact/react-form-practice-ceLWEsp
-
+//https://scrimba.com/learn/learnreact/react-container-component-architecture-cwRZ9fG
 // https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
 
 import React, { Component } from "react";
